401d99d14931346954a03c5eb75d43ef066c7486
sebafermanelli <sebastianfermanelli@gmail.com>
2024-05-26 23:44:37 -0300

    update

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://electoral-app-backend-production.up.railway.app/api/",
+    baseURL: import.meta.env.API_URL,
 });
